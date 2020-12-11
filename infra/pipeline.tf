6b0ea5f7815089a3003a1c50b48583b3e51f03de
Max Krivich <maxkrivich@gmail.com>
2020-12-11 09:06:10 +0100

    Add basic pipeline for heroku

---
+++ infra/pipeline.tf
@@ -0,0 +1,17 @@
+# Heroku pipeline
+
+resource "heroku_pipeline" "pipeline" {
+  name = var.heroku_pipeline_name
+}
+
+resource "heroku_pipeline_coupling" "staging" {
+  app      = heroku_app.staging.name
+  pipeline = heroku_pipeline.pipeline.id
+  stage    = "staging"
+}
+
+resource "heroku_pipeline_coupling" "production" {
+  app      = heroku_app.production.name
+  pipeline = heroku_pipeline.pipeline.id
+  stage    = "production"
+}
